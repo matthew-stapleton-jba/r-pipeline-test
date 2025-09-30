d7b08ad09727823851496e348bc3f53b0db200ec
Matthew Stapleton <matthew.stapleton@jbaconsulting.com>
2025-09-30 16:03:54 +0100

    add pages

--- R/main.R
+++ R/main.R
@@ -1,1 +1,15 @@
+#' Add two numbers
+#'
+#' Computes the sum of two numeric values.
+#'
+#' @param x A numeric value.
+#' @param y A numeric value.
+#'
+#' @return A numeric value giving the sum of \code{x} and \code{y}.
+#' @examples
+#' add_two(1, 2)
+#' add_two(10, 5)
+#'
+#' @export
 add_two <- function(x, y) x + y
+
