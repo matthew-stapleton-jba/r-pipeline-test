188682ed5f0e4486a37af766f4e490588754fe22
matthew.stapleton@jbaconsulting.com <matthew.stapleton@jbaconsulting.com>
2025-10-01 14:24:12 +0100

    add paired down version

---
+++ RPipelineTest/tests/testthat/test-basic.R
@@ -0,0 +1,3 @@
+test_that("math works", {
+  expect_equal(1 + 1, 2)
+})
--- tests/testthat/test-main.R
+++
@@ -1,5 +0,0 @@
-test_that("add_two adds", {
-  expect_equal(add_two(1, 2), 3)
-  expect_equal(add_two(-1, 1), 0)
-  expect_equal(add_two(0, 0), 0)
-})
